496864e1ae38b68cd0c52f410f5fe591785f7e66
unknown <ilhamksyuriadi@gmail.com>
2018-08-14 17:07:02 +0700

    -

--- PNN.py
+++ PNN.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-#from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d import Axes3D
 from sklearn.model_selection import train_test_split
 
 fig = plt.figure()
